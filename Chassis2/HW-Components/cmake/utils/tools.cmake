5a219adc455750be1e9e04e6b9d6c131ce212c5d
Wpy <wpy5318280@163.com>
2025-02-23 17:21:43 +0800

    refactor：重新引入两个文件夹的HW_Components作为submodules并更新至合适版本

--- Chassis2/HW-Components/cmake/utils/tools.cmake
+++
@@ -1,2 +0,0 @@
-include(${HWC_CMAKE_MODULE_PATH}/utils/macro_tools.cmake)
-include(${HWC_CMAKE_MODULE_PATH}/utils/function_tools.cmake)
--- Chassis2/HW-Components/modules/README.md
+++
@@ -1,5 +0,0 @@
-# Modules 机器人模块组件
-
-Copyright (c) 2025 Hello World Team, Zhejiang University.
-
-All Rights Reserved.
